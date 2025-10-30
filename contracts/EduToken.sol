acd0e14c686f32180ddec8931e8c6761dda80d1f
revanth58 <revanthk7777@gmail.com>
2025-10-30 09:08:49 +0530

    adding edutoken.sol

--- contracts/EduToken.sol
+++ contracts/EduToken.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract EduToken is ERC20 {
+    address public admin;
+
+    constructor(uint256 initialSupply) ERC20("EduToken", "EDU") {
+        _mint(msg.sender, initialSupply);
+        admin = msg.sender;
+    }
+
+    /// @notice Mint reward tokens to a student (only admin)
+    function mintReward(address student, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can mint rewards");
+        _mint(student, amount);
+    }
+
+    /// @notice Redeem tokens (burn from caller’s balance)
+    function redeem(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    /// @notice Transfer admin rights to a new address
+    function changeAdmin(address newAdmin) external {
+        require(msg.sender == admin, "Only current admin can change admin");
+        require(newAdmin != address(0), "New admin cannot be zero address");
+        admin = newAdmin;
+    }
+}
